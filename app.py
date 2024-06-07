0101e85a223c74c19711f2a4905a940386d6bbbe
mrdjangodev <mirolimcoder@gmail.com>
2024-06-07 14:42:28 +0500

    Parameterized Routing done

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
 from webob import Request, Response
-
+from parse import parse
 
 class DastyorApp:
+    def __init__(self):
+        self.routes = dict()
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
 
     def handle_request(self, request):
-        user_agent = request.environ.get("HTTP_USER_AGENT", "User Agent Not Found")
         response = Response()
-        response.text = f"Assalomu alaykum my friend with user agent {user_agent}"
+        handler, kwargs = self.find_handler(request)
+        if handler is not None: 
+            handler(request, response, **kwargs)
+        else:
+            self.default_response(response)
+
         return response
+
+    def find_handler(self, request):
+        for path, handler in self.routes.items():
+            parsed_result = parse(path, request.path)
+
+            if parsed_result is not None:
+                return handler, parsed_result.named
+        return None, None
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Page not found."
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        return wrapper
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from app import DastyorApp
 
 app = DastyorApp()
+
+@app.route("/")
+def home(request, response):
+    response.text = "Hey from Home page"
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Message from About page"
+
+
+@app.route("/welcome/{name}")
+def welcome(request, response, name):
+    response.text = f"Welcome {name}"
